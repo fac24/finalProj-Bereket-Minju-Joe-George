78dd397373047b5d7b89f8ed86342530bf6d2797
glombort <george.lambert99@gmail.com>
2022-06-27 10:50:59 +0100

    NOT NULL
    Co-Authored-By: Bereket Mebrahtu <47576080+Bereketmebrahtu@users.noreply.github.com>
    Co-Authored-By: Minju Kim <48757517+minju25kim@users.noreply.github.com>
    Co-Authored-By: Joe <94115299+joe-dev-public@users.noreply.github.com>

--- database/init.sql
+++ database/init.sql
@@ -4,39 +4,42 @@ DROP TABLE IF EXISTS stations, lines, platforms, platform_exits, exit_interchang
 
 CREATE TABLE stations(
   id SERIAL PRIMARY KEY,
-  name TEXT
+  name TEXT NOT NULL
 );
 
 CREATE TABLE lines(
   id SERIAL PRIMARY KEY,
-  name TEXT
+  name TEXT NOT NULL
 );
 
 CREATE TABLE platforms (
   id SERIAL PRIMARY KEY,
-  name TEXT,
-  train_direction TEXT,
-  station_id INTEGER REFERENCES stations(id),
-  line_id INTEGER REFERENCES lines(id)
+  name TEXT NOT NULL,
+  train_direction TEXT NOT NULL,
+  station_id INTEGER REFERENCES stations(id) NOT NULL,
+  line_id INTEGER REFERENCES lines(id) NOT NULL
 );
 
 CREATE TABLE platform_exits (
   id SERIAL PRIMARY KEY,
-  carriage INTEGER,
-  door INTEGER,
-  type INTEGER
+  carriage INTEGER NOT NULL,
+  door INTEGER NOT NULL,
+  type INTEGER NOT NULL
 );
 
 CREATE TABLE exit_interchanges (
   id SERIAL PRIMARY KEY,
-  platform_exit_id INTEGER REFERENCES platform_exits(id),
-  dest_platform_id INTEGER REFERENCES platforms(id)
+  platform_exit_id INTEGER REFERENCES platform_exits(id) NOT NULL,
+  dest_platform_id INTEGER REFERENCES platforms(id) NOT NULL
 );
 
 CREATE TABLE station_lines (
   id SERIAL PRIMARY KEY,
-  station_id INTEGER REFERENCES stations(id),
-  line_id INTEGER REFERENCES lines(id)
+  station_id INTEGER REFERENCES stations(id) NOT NULL,
+  line_id INTEGER REFERENCES lines(id) NOT NULL
 );
+
+
+-- INSERT INTO station (name, id) VALUES ()
 
 COMMIT;
