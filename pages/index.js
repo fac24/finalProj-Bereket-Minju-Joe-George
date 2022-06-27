42ba816269d43b93b726687900686a6f59db14a2
Minju25Kim <48757517+minju25kim@users.noreply.github.com>
2022-06-27 10:46:26 +0100

    add tailwind

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <h1>Hello world!</h1>
+        <h1 className="text-3xl font-bold underline">hello world!</h1>
       </main>
 
       <footer></footer>
