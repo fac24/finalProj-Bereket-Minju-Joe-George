f0b1a490621154ece4dfa51b732c1be3f4ae1e5e
joe <joe.dev.public@gmail.com>
2022-06-29 14:27:05 +0100

    Temporarily comment most stuff and change stations table

--- database/init.sql
+++ database/init.sql
@@ -2,50 +2,51 @@ BEGIN;
 
 DROP TABLE IF EXISTS stations, lines, platforms, platform_exits, exit_interchanges, station_lines CASCADE;
 
-CREATE TABLE stations(
+CREATE TABLE stations (
   id SERIAL PRIMARY KEY,
-  name TEXT NOT NULL
+  station_naptan TEXT NOT NULL,
+  common_name_short TEXT NOT NULL
 );
 
-CREATE TABLE lines(
-  id SERIAL PRIMARY KEY,
-  name TEXT NOT NULL
-);
+-- CREATE TABLE lines (
+--   id SERIAL PRIMARY KEY,
+--   name TEXT NOT NULL
+-- );
 
-CREATE TABLE platforms (
-  id SERIAL PRIMARY KEY,
-  name TEXT NOT NULL,
-  train_direction TEXT NOT NULL,
-  station_id INTEGER REFERENCES stations(id) NOT NULL,
-  line_id INTEGER REFERENCES lines(id) NOT NULL
-);
+-- CREATE TABLE platforms (
+--   id SERIAL PRIMARY KEY,
+--   name TEXT NOT NULL,
+--   train_direction TEXT NOT NULL,
+--   station_id INTEGER REFERENCES stations(id) NOT NULL,
+--   line_id INTEGER REFERENCES lines(id) NOT NULL
+-- );
 
-CREATE TABLE platform_exits (
-  id SERIAL PRIMARY KEY,
-  carriage INTEGER NOT NULL,
-  door INTEGER NOT NULL,
-  type INTEGER NOT NULL
-);
+-- CREATE TABLE platform_exits (
+--   id SERIAL PRIMARY KEY,
+--   carriage INTEGER NOT NULL,
+--   door INTEGER NOT NULL,
+--   type INTEGER NOT NULL
+-- );
 
-CREATE TABLE users_feedback (
-  id SERIAL PRIMARY KEY,
-  platform_exits_id INTEGER REFERENCES platform_exits(id),
-  carriage INTEGER NOT NULL,
-  door INTEGER NOT NULL,
-  votes INTEGER NOT NULL
-);
+-- CREATE TABLE users_feedback (
+--   id SERIAL PRIMARY KEY,
+--   platform_exits_id INTEGER REFERENCES platform_exits(id),
+--   carriage INTEGER NOT NULL,
+--   door INTEGER NOT NULL,
+--   votes INTEGER NOT NULL
+-- );
 
-CREATE TABLE exit_interchanges (
-  id SERIAL PRIMARY KEY,
-  platform_exit_id INTEGER REFERENCES platform_exits(id) NOT NULL,
-  dest_platform_id INTEGER REFERENCES platforms(id) NOT NULL
-);
+-- CREATE TABLE exit_interchanges (
+--   id SERIAL PRIMARY KEY,
+--   platform_exit_id INTEGER REFERENCES platform_exits(id) NOT NULL,
+--   dest_platform_id INTEGER REFERENCES platforms(id) NOT NULL
+-- );
 
-CREATE TABLE station_lines (
-  id SERIAL PRIMARY KEY,
-  station_id INTEGER REFERENCES stations(id) NOT NULL,
-  line_id INTEGER REFERENCES lines(id) NOT NULL
-);
+-- CREATE TABLE station_lines (
+--   id SERIAL PRIMARY KEY,
+--   station_id INTEGER REFERENCES stations(id) NOT NULL,
+--   line_id INTEGER REFERENCES lines(id) NOT NULL
+-- );
 
 
 -- INSERT INTO station (name, id) VALUES ()
