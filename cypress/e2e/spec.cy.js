f0083dd39cd6874807600523f3beafa75f1a2f68
bereket <mebrahtubereket@gmail.com>
2022-07-05 16:03:17 +0100

    added more tests

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -56,6 +56,25 @@ describe("select one of the start station options", () => {
   });
 });
 
+describe("type station name and select one of the searched results", () => {
+  it("type station name", () => {
+    cy.get("#startStation").type("Ang");
+  });
+  it("view related searched results", () => {
+    cy.get("#react-select-3-listbox")
+      .find(".css-4ljt47-MenuList")
+      .find("div")
+      .first()
+      .contains("Angel");
+  });
+  it("can select one of the options", () => {
+    cy.get("#react-select-3-option-5").click();
+  });
+  it("can check selected option", () => {
+    cy.get(".css-qc6sy-singleValue").contains("Angel");
+  });
+});
+
 //end station click select
 describe("click to view end station options", () => {
   it("can view options", () => {
