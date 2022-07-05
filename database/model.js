a6ffaac514c8527800a6d61b9a7bd0bcc7510a29
glombort <george.lambert99@gmail.com>
2022-07-05 11:10:41 +0100

    Order the queries
    Ordered the queries that use any with the idx function
    
    Co-Authored-By: Minju Kim <48757517+minju25kim@users.noreply.github.com>

--- database/model.js
+++ database/model.js
@@ -118,6 +118,7 @@ async function getPlatformDataFromIndividualStopPoints(stopIds) {
     FROM platforms, lines, platform_line
     WHERE (lines.id = platform_line.line_id AND platforms.id = platform_line.platform_id)
     AND platforms.individual_stop_id = ANY ($1)
+    ORDER BY idx($1, station_naptan)
   `;
   const platformData = await db.query(SELECT_PLATFORM_DATA, [stopIds]);
   return platformData.rows;
@@ -128,6 +129,7 @@ async function getTrainDirectionFromIndividualStopPoints(stopIds) {
     SELECT train_direction
     FROM platforms
     WHERE individual_stop_id = ANY ($1)
+    ORDER BY idx($1, station_naptan)
   `;
   const trainDirections = await db.query(SELECT_TRAIN_DIRECTION, [stopIds]);
   return trainDirections.rows;
