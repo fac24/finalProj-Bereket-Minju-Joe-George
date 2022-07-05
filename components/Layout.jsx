b499c51043fbacf37598fc58d4146e3678673751
glombort <george.lambert99@gmail.com>
2022-07-05 13:03:24 +0100

    Styled layout
    Styled the layout page
    
    Co-Authored-By: Joe <94115299+joe-dev-public@users.noreply.github.com>

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,31 +1,31 @@
 import Head from "next/head";
 import Link from "next/link";
+import MainNavButton from "./MainNavButton";
 
 export default function Layout({ children }) {
   return (
-    <div>
+    <>
       <Head>
         <title>Train Exits</title>
       </Head>
-      <header className="border-b-8">
-        <Link href="/">
-          <a className="text-3xl font-bold underline">Train Exits</a>
-        </Link>
-        <br />
-        <Link href="/">
-          <a>New Route</a>
-        </Link>
-        <Link href="/saved-routes">
-          <a>Saved Routes</a>
-        </Link>
-      </header>
-      <main>{children}</main>
-      <footer className="text-sm text-center">
-        <p>All rights reserved.</p>
-        <p>
-          Copyright <br className="md:hidden"></br>© 2022 FAC24 final project
-        </p>
-      </footer>
-    </div>
+      <div className="mx-auto max-w-xs pt-5">
+        <header className="text-center mb-6">
+          <h1 className="mb-3">
+            <Link href="/">
+              <a className="text-3xl font-bold">Train Exits</a>
+            </Link>
+          </h1>
+          <nav className="flex gap-x-2 justify-center">
+            <MainNavButton href="/" text="New route" />
+            <MainNavButton href="/saved-routes" text="Saved routes" />
+          </nav>
+        </header>
+        <main>{children}</main>
+        <footer className="text-sm text-center mt-6 mb-4 pt-3 border-t">
+          <p>FAC24 final project</p>
+          <p>Bereket, George, Joe, Minju</p>
+        </footer>
+      </div>
+    </>
   );
 }
