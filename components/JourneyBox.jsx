c846efce14e3d5612b866ce2c9f6d3d1003af3da
glombort <george.lambert99@gmail.com>
2022-07-07 11:40:57 +0100

    Update JourneyBox.jsx

--- components/JourneyBox.jsx
+++ components/JourneyBox.jsx
@@ -9,19 +9,21 @@ export default function JourneyBox({ journey, startEndNames }) {
     return (
       <div key={index} className="block relative">
         <div className="inline-block text-lg">
-          <div className="flex">
-            <div className="rounded-full border-4 border-black h-4">&nbsp;</div>
-            <span>{departurePoint}</span>
+          <div className="flex content-end">
+            <div className="rounded-full border-4 border-black h-5 w-5 m-auto">
+              &nbsp;
+            </div>
+            <span className="ml-2 mt-1">{departurePoint}</span>
           </div>
-          <div className="h-full">
-            <span className={lineColour}>&nbsp;</span>
+          <div className="h-7">
+            <span className={`text-4xl ml-1.5 ${lineColour}`}>&nbsp;</span>
           </div>
           {index === arr.length - 1 ? (
-            <div className="flex">
-              <div className="rounded-full border-4 border-black h-4">
+            <div className="flex content-end">
+              <div className="rounded-full border-4 border-black h-5 w-5 mt-2.5">
                 &nbsp;
               </div>
-              <span>{arrivalPoint}</span>
+              <span className="ml-2 mt-1.5">{arrivalPoint}</span>
             </div>
           ) : null}
         </div>
