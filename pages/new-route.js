6aeb512f433a0a1efdb5d1303c7508b81853c0db
glombort <george.lambert99@gmail.com>
2022-07-07 17:09:46 +0100

    Remove duplicates and walking only

--- pages/new-route.js
+++ pages/new-route.js
@@ -54,81 +54,99 @@ export default function NewRoute({
       <>
         <FromToVia from={startEndNames.start} to={startEndNames.end} />
         <ul id="all-possible-routes">
-          {apiResponseData.journeys.map(
-            (
-              journey,
-              index // take dataResponse.journeys to map journey and render as list items
-            ) => {
-              const href = journey.legs.map(
-                (leg) =>
-                  `${leg.departurePoint.individualStopId},${leg.arrivalPoint.individualStopId}`
-              );
-              return (
-                <li className="my-4 border route-option" key={index}>
-                  <form
-                    key={index}
-                    action="../api/add-saved-route"
-                    method="POST"
-                    className="route-option-form flex p-4 cursor-pointer"
-                    onClick={(event) => event.currentTarget.submit()}
-                  >
-                    <input
-                      type="hidden"
-                      name="href"
-                      value={`/show-route?startStationNaptan=${
-                        urlParams.startStation
-                      }&endStationNaptan=${
-                        urlParams.endStation
-                      }&viaStationNaptans=${journey.legs.map(
-                        (leg, index, arr) => {
-                          if (index !== arr.length - 1) {
-                            return leg.arrivalPoint.naptanId;
+          {apiResponseData.journeys
+            .filter((journey, index, arr) => {
+              if (journey.legs[journey.legs.length - 1].mode.id === "walking")
+                return false;
+              return true;
+            })
+            .filter((journey, index, arr) => {
+              for (let i = index + 1; i < arr.length; i++) {
+                for (let j = 0; j < journey.legs.length; j++) {
+                  if (
+                    journey.legs[j].path.lineString ===
+                    arr[i].legs[j].path.lineString
+                  )
+                    return false;
+                }
+              }
+              return true;
+            })
+            .map(
+              (
+                journey,
+                index // take dataResponse.journeys to map journey and render as list items
+              ) => {
+                const href = journey.legs.map(
+                  (leg) =>
+                    `${leg.departurePoint.individualStopId},${leg.arrivalPoint.individualStopId}`
+                );
+                return (
+                  <li className="my-4 border route-option" key={index}>
+                    <form
+                      key={index}
+                      action="../api/add-saved-route"
+                      method="POST"
+                      className="route-option-form flex p-4 cursor-pointer"
+                      onClick={(event) => event.currentTarget.submit()}
+                    >
+                      <input
+                        type="hidden"
+                        name="href"
+                        value={`/show-route?startStationNaptan=${
+                          urlParams.startStation
+                        }&endStationNaptan=${
+                          urlParams.endStation
+                        }&viaStationNaptans=${journey.legs.map(
+                          (leg, index, arr) => {
+                            if (index !== arr.length - 1) {
+                              return leg.arrivalPoint.naptanId;
+                            }
                           }
-                        }
-                      )}&individualStopIds=${href.join(",")}`}
-                    />
-                    <div className="mr-4">
-                      {journey.legs.map(
-                        (
-                          leg,
-                          index // map data to list journey
-                        ) => (
-                          // Todo: replace these text labels with colourful rectangles :)
-                          // Use the line name as hidden text for a11y
-                          // Maybe use a table so we can vertically align interchanges and lines.
-                          <div key={index}>
-                            <HiddenInputs leg={leg} />
-                            <span
-                              className={
-                                leg.routeOptions[0].lineIdentifier?.id +
-                                " block px-1.5 py-1"
-                              }
-                            >
-                              {leg.routeOptions[0].lineIdentifier?.name}
-                            </span>
-                          </div>
-                        )
-                      )}
-                    </div>
-                    <ul>
-                      {journey.legs.map((leg, index, arr) =>
-                        leg.routeOptions[0].lineIdentifier?.id ===
-                        undefined ? null : arr.length === 1 ? (
-                          <li key={index}>Direct</li>
-                        ) : /* <li key={index}>{leg.instruction.summary}</li> */
-                        // For all legs except the last, print the arrival station name,
-                        // as this is equivalent to the "interchange" stop.
-                        // (The arrival station of leg 1 will be the same as the departure station of leg 2, etc.)
-                        index !== arr.length - 1 ? ( // if this is the last leg then list journey leg arrivalPoint name
-                          <li key={index}>{leg.arrivalPoint.commonName}</li>
-                        ) : null
-                      )}
-                    </ul>
-                  </form>
-                </li>
-              );
-            }
-          )}
+                        )}&individualStopIds=${href.join(",")}`}
+                      />
+                      <div className="mr-4">
+                        {journey.legs.map(
+                          (
+                            leg,
+                            index // map data to list journey
+                          ) => (
+                            // Todo: replace these text labels with colourful rectangles :)
+                            // Use the line name as hidden text for a11y
+                            // Maybe use a table so we can vertically align interchanges and lines.
+                            <div key={index}>
+                              <HiddenInputs leg={leg} />
+                              <span
+                                className={
+                                  leg.routeOptions[0].lineIdentifier?.id +
+                                  " block px-1.5 py-1"
+                                }
+                              >
+                                {leg.routeOptions[0].lineIdentifier?.name}
+                              </span>
+                            </div>
+                          )
+                        )}
+                      </div>
+                      <ul>
+                        {journey.legs.map((leg, index, arr) =>
+                          leg.routeOptions[0].lineIdentifier?.id ===
+                          undefined ? null : arr.length === 1 ? (
+                            <li key={index}>Direct</li>
+                          ) : /* <li key={index}>{leg.instruction.summary}</li> */
+                          // For all legs except the last, print the arrival station name,
+                          // as this is equivalent to the "interchange" stop.
+                          // (The arrival station of leg 1 will be the same as the departure station of leg 2, etc.)
+                          index !== arr.length - 1 ? ( // if this is the last leg then list journey leg arrivalPoint name
+                            <li key={index}>{leg.arrivalPoint.commonName}</li>
+                          ) : null
+                        )}
+                      </ul>
+                    </form>
+                  </li>
+                );
+              }
+            )}
         </ul>
       </>
     );
